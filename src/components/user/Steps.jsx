edce34e3c51e62a1bea62c3d849286f16d563553
Andrik Pineda <kean5817@gmail.com>
2020-11-27 12:42:58 -0600

    Creando form para la creacion de publicacion

--- src/components/user/Steps.jsx
+++ src/components/user/Steps.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { TextField, FormControl, InputLabel, Select } from "@material-ui/core";
+import {
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  Grid,
+} from "@material-ui/core";
 import { MenuItem } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     "& > *": {
-      margin: theme.spacing(3),
+      margin: theme.spacing(2),
     },
   },
   container: {
-    width: "100%",
     display: "flex",
-    alignItems: "center",
+    flexWrap: "wrap",
     justifyContent: "center",
+    marginLeft: "150px",
+    marginRight: "100px",
   },
   formControl: {
-    margin: theme.spacing(3),
-    minWidth: 200,
+    margin: theme.spacing(2),
+    width: 200,
   },
   selectEmpty: {
     marginTop: theme.spacing(2),
@@ -29,55 +36,24 @@ export const StepOne = ({ handleChange, product }) => {
 
   console.log(product);
   return (
-    <div className={classes.container}>
-      <form className={classes.root}>
-        <TextField
-          label="Nombre"
-          name="name"
-          value={product.name}
-          onChange={handleChange}
-        />
-        <TextField
+    <form className={classes.root}>
+      <Grid container spacing={2} className={classes.root}>
+        <Grid item xs>
+          <TextField
+            label="Nombre"
+            name="name"
+            value={product.name}
+            onChange={handleChange}
+          />
+          <TextField
           label="Marca"
           name="brand"
           value={product.brand}
           onChange={handleChange}
         />
-        <TextField
-          label="Descripción"
-          name="description"
-          value={product.description}
-          onChange={handleChange}
-        />
-        <FormControl className={classes.formControl}>
-        <InputLabel id="demo-simple-select-label">Categoria</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          name="category"
-          value={product.category}
-          onChange={handleChange}
-        >
-          <MenuItem value={"Electronica"}>Electronica</MenuItem>
-          <MenuItem value={"Casa y Hogar"}>Casa y Hogar</MenuItem>
-          <MenuItem value={"Vehiculos"}>Vehiculos</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl className={classes.formControl}>
-        <InputLabel id="demo-simple-select">Condicion</InputLabel>
-        <Select
-          labelId="demo-simple-select"
-          id="demo-simple-select1"
-          name="state"
-          value={product.state}
-          onChange={handleChange}
-        >
-          <MenuItem value={"new"}>Nuevo</MenuItem>
-          <MenuItem value={"used"}>Usado</MenuItem>
-        </Select>
-      </FormControl>
-      </form>
-    </div>
+        </Grid>
+      </Grid>
+    </form>
   );
 };
 
