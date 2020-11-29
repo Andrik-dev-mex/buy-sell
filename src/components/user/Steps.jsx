2195f88d42033927f38d7424c26ee4c47cd9b8fb
Andrik Pineda <kean5817@gmail.com>
2020-11-28 21:39:38 -0600

    Terminando formulario de publicaciones

--- src/components/user/Steps.jsx
+++ src/components/user/Steps.jsx
@@ -6,6 +6,8 @@ import {
   InputLabel,
   Select,
   Grid,
+  Paper,
+  Radio
 } from "@material-ui/core";
 import { MenuItem } from "@material-ui/core";
 
@@ -15,44 +17,108 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(2),
     },
   },
-  container: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    marginLeft: "150px",
-    marginRight: "100px",
-  },
   formControl: {
-    margin: theme.spacing(2),
-    width: 200,
+    margin: theme.spacing(0),
   },
   selectEmpty: {
-    marginTop: theme.spacing(2),
+    marginTop: theme.spacing(1),
+  },
+  container: {
+    padding: "1em",
   },
 }));
 
 export const StepOne = ({ handleChange, product }) => {
   const classes = useStyles();
-
   console.log(product);
   return (
     <form className={classes.root}>
-      <Grid container spacing={2} className={classes.root}>
-        <Grid item xs>
-          <TextField
-            label="Nombre"
-            name="name"
-            value={product.name}
-            onChange={handleChange}
-          />
-          <TextField
-          label="Marca"
-          name="brand"
-          value={product.brand}
-          onChange={handleChange}
-        />
+      <Paper elevation={3}>
+        <Grid container className={classes.container}>
+          <Grid item xs={12}>
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <TextField
+                  label="Nombre"
+                  name="name"
+                  value={product.name}
+                  onChange={handleChange}
+                  fullWidth
+                />
+                <TextField
+                  label="Marca"
+                  name="brand"
+                  value={product.brand}
+                  onChange={handleChange}
+                  fullWidth
+                />
+                <TextField
+                  label="Descripción"
+                  name="description"
+                  value={product.description}
+                  onChange={handleChange}
+                  fullWidth
+                />
+                <FormControl className={classes.formControl} fullWidth>
+                  <InputLabel id="demo-simple-select-label">
+                    Categoria
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    name="category"
+                    value={product.category}
+                    onChange={handleChange}
+                  >
+                    <MenuItem value={"Electronica"}>Electronica</MenuItem>
+                    <MenuItem value={"Casa y Hogar"}>Casa y Hogar</MenuItem>
+                    <MenuItem value={"Vehiculos"}>Vehiculos</MenuItem>
+                  </Select>
+                </FormControl>
+                <FormControl className={classes.formControl} fullWidth>
+                  <InputLabel id="demo-simple-select">Condicion</InputLabel>
+                  <Select
+                    labelId="demo-simple-select"
+                    id="demo-simple-select1"
+                    name="state"
+                    value={product.state}
+                    onChange={handleChange}
+                  >
+                    <MenuItem value={"new"}>Nuevo</MenuItem>
+                    <MenuItem value={"used"}>Usado</MenuItem>
+                  </Select>
+                </FormControl>
+                <TextField
+                  label="Precio"
+                  name="price"
+                  value={product.price}
+                  type={"number"}
+                  onChange={handleChange}
+                  InputProps={{ min: 1 }}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={8}>
+                <Radio
+                  checked={product.typeOfBuy === "buy"}
+                  onChange={handleChange}
+                  value="buy"
+                  name="typeOfBuy"
+                  inputProps={{ "aria-label": "A" }}
+                />
+                <Radio
+                  label="Cambio"
+                  checked={product.typeOfBuy === "change"}
+                  onChange={handleChange}
+                  value="change"
+                  name="typeOfBuy"
+                  inputProps={{ "aria-label": "Cambio" }}
+                />
+              </Grid>
+            </Grid>
+          </Grid>
         </Grid>
-      </Grid>
+      </Paper>
     </form>
   );
 };
